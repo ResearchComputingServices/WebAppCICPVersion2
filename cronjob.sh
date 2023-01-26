1b1deaeec2c6453cf95efca79e6d25ad427f6f77
Ryan Taylor <ryan.taylor@carleton.ca>
2023-01-26 10:34:30 -0500

    Fixed to requirements.txt and cronjob

--- cronjob.sh
+++ cronjob.sh
@@ -6,7 +6,4 @@ DJANGO_DIR=$(dirname $(realpath $0))
 cd "$DJANGO_DIR"
 source cicp_env/bin/activate
 
-echo "Extracting User Data" >& /tmp/cronjob_lastrun.log
-python3 manage.py runscript ExtractUserData 2>&1 >> /tmp/cronjob_lastrun.log
-echo "Extracting Survey Data"  2>&1 >> /tmp/cronjob_lastrun.log
-python3 manage.py runscript ExtractSurvey 2>&1 >> /tmp/cronjob_lastrun.log
+python3 manage.py runscript cronJobScript >& /tmp/cronjob_lastrun.log
