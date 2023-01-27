b92f7b1bb1d87cb9572d5b397b704115a9ad9330
Nicholi Shiell <nicholishiell@gmail.com>
2023-01-27 08:58:58 -0500

    added a function to auto fill some test survey data

--- scripts/cronJobScript.py
+++ scripts/cronJobScript.py
@@ -35,15 +35,29 @@ def HandleReleasedSurvey(currentSurvey, currentDate):
 #   and which do not have an accessedDate field defined.
 # - after Fetching and Extracting the survey entities accessedDate field will be set to the current date
 ################################################################################################################################## 
+def AddSurvey(id,date):
+    survey = SurveyTable()
+    survey.qualtricsSurveyID = id
+    survey.releaseDate = date
+    survey.save()
+
+
 def run(*args):
+    ##########################################################
+    # TODO: Remove this block before production!
+    AddSurvey('SV_4PKhCR2n0Hw6xbo','2022-12-01')
+    AddSurvey('SV_aYnUZN7y2nQhXJc','2022-12-17')
+    AddSurvey('SV_0eMEVIZkeSbKyjk','2023-01-03')
+    ##########################################################
+    
     surveyQuerySet = SurveyTable.objects.all()
     
-    currentDate = today = date.today()
+    currentDate = date.today()
     
     print(currentDate)
     
     for survey in surveyQuerySet:
-        print(survey.surveyID,'\n',survey.qualtricsSurveyID,'\n',survey.releaseDate,'\n',survey.accessedDate)
+        print(survey.id,'\n',survey.qualtricsSurveyID,'\n',survey.releaseDate,'\n',survey.accessedDate)
         
         if currentDate >= survey.releaseDate:
             HandleReleasedSurvey(survey,currentDate)
