9b7dd9692af2c23fa74e0bc5327e8921a535bf43
Nicholi Shiell <nicholishiell@gmail.com>
2023-05-30 12:03:59 -0400

    Changes in templates

--- static/js/index.js
+++ static/js/index.js
@@ -1,24 +1,22 @@
-$(document).ready(function () {
+function toggleblackbox() {
+
+    e.preventDefault();
+    // get the clock
+    var myBox = document.getElementById('blackbox');
+
+    // get the current value of the clock's display property
+    var displaySetting = myBox.style.display;
 
 
-    function toggleblackbox() {
-        // get the clock
-        var myBox = document.getElementById('blackbox');
-
-        // get the current value of the clock's display property
-        var displaySetting = myBox.style.display;
-
-
-        // now toggle the clock and the button text, depending on current state
-        if (displaySetting == 'block') {
-            // clock is visible. hide it
-            myBox.style.display = 'none';
-        }
-        else {
-            // clock is hidden. show it
-            myBox.style.display = 'block';
-        }
+    // now toggle the clock and the button text, depending on current state
+    if (displaySetting == 'block') {
+        // clock is visible. hide it
+        myBox.style.display = 'none';
     }
+    else {
+        // clock is hidden. show it
+        myBox.style.display = 'block';
+    }
+}
 
-});
 
