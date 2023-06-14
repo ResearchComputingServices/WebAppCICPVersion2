69a26c9f9bcc95a08f71cbf8902219a29cc7f232
Nicholi Shiell <nicholishiell@gmail.com>
2023-06-14 13:33:47 -0400

    Changes to landing page

--- static/js/lpage.js
+++ static/js/lpage.js
@@ -1,12 +1,14 @@
-var messageArray = ["Charity Insights Canada Project"];
+var messageArray = ["Charity Insights Canada Project", "Inform", "Build", "Strengthen"];
 var textPosition = 0;
 var speed = 100;
 
 
 typewriter = () => {
-    document.querySelector('#message').innerHTML = messageArray[0].substring(0, textPosition) + "<span>\u25ae</span>";
+    document.querySelector('#message').innerHTML = messageArray[0].substring(0, textPosition)
+    document.querySelector("#message-dynamic").innerHTML = messageArray[1].substring(0, textPosition) + messageArray[2].substring(0, textPosition) + messageArray[3].substring(0, textPosition) + "<span>\u25ae</span>";
 
-    if (textPosition++ != messageArray[0].length)
+
+    if (textPosition++ != messageArray.length)
         setTimeout(typewriter, speed);
 }
 
