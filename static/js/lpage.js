4d11896f6704f534e153988956ccf354f93e2d58
Nicholi Shiell <nicholishiell@gmail.com>
2023-06-14 11:39:18 -0400

    Changes to landing page

--- static/js/lpage.js
+++ static/js/lpage.js
@@ -1,26 +1,3 @@
-// const carouselText = [
-//     { text: "Inform", color: "red" },
-//     { text: "Build", color: "red" },
-//     { text: "Strengthen", color: "red" }
-// ]
-
-// async function carousel(carouselList, eleRef) {
-//     var i = 0;
-//     while (true) {
-//         updateFontColor(eleRef, carouselList[i].color)
-//         await typeSentence(carouselList[i].text, eleRef);
-//         await waitForMs(1500);
-//         await deleteSentence(eleRef);
-//         await waitForMs(500);
-//         i++
-//         if (i >= carouselList.length) { i = 0; }
-//     }
-// }
-
-// function updateFontColor(eleRef, color) {
-//     $(eleRef).css('color', color);
-// }
-
 var messageArray = ["Charity Insights Canada Project"];
 var textPosition = 0;
 var speed = 100;
