6bdaf235589a1b69c8e7afcc0743923c18787792
Nicholi Shiell <nicholishiell@gmail.com>
2023-06-14 10:39:58 -0400

    Changes to landing page

--- static/js/lpage.js
+++ static/js/lpage.js
@@ -1,61 +1,73 @@
-$(document).ready(async function () {
-    await typeSentence("Charity Insights Canada Project", "#sentence");
-    await waitForMs(2000);
-    carousel(carouselText, "#sentence");
-});
+// $(document).ready(async function () {
+//     await typeSentence("Charity Insights Canada Project", "#sentence");
+//     await waitForMs(2000);
+//     carousel(carouselText, "#sentence");
+// });
 
-typeSentence("Charity Insights Canada Project", "#sentence");
+// typeSentence("Charity Insights Canada Project", "#sentence");
 
-async function typeSentence(sentence, eleRef, delay = 100) {
-    console.log("Inside type sentence")
-    const letters = sentence.split("");
-    let i = 0;
-    while (i < letters.length) {
-        await waitForMs(delay);
-        $(eleRef).append(letters[i]);
-        i++
-    }
-    return;
+// async function typeSentence(sentence, eleRef, delay = 100) {
+//     console.log("Inside type sentence")
+//     const letters = sentence.split("");
+//     let i = 0;
+//     while (i < letters.length) {
+//         await waitForMs(delay);
+//         $(eleRef).append(letters[i]);
+//         i++
+//     }
+//     return;
+// }
+
+
+// function waitForMs(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms))
+// }
+
+// async function deleteSentence(eleRef) {
+//     const sentence = $(eleRef).html();
+//     const letters = sentence.split("");
+//     let i = 0;
+//     while (letters.length > 0) {
+//         await waitForMs(100);
+//         letters.pop();
+//         $(eleRef).html(letters.join(""));
+//     }
+// }
+
+
+// const carouselText = [
+//     { text: "Inform", color: "red" },
+//     { text: "Build", color: "red" },
+//     { text: "Strengthen", color: "red" }
+// ]
+
+// async function carousel(carouselList, eleRef) {
+//     var i = 0;
+//     while (true) {
+//         updateFontColor(eleRef, carouselList[i].color)
+//         await typeSentence(carouselList[i].text, eleRef);
+//         await waitForMs(1500);
+//         await deleteSentence(eleRef);
+//         await waitForMs(500);
+//         i++
+//         if (i >= carouselList.length) { i = 0; }
+//     }
+// }
+
+// function updateFontColor(eleRef, color) {
+//     $(eleRef).css('color', color);
+// }
+
+var messageArray = ["Charity Insights Canada Project"];
+var textPosition = 0;
+var speed = 100;
+
+
+typewriter = () => {
+    document.querySelector('#message').innerHTML = messageArray[0].substring(0, textPosition) + "<span>/u25ae</span>";
+
+    if (textPosition++ != messageArray[0].length)
+        setTimeout(typewriter, speed);
 }
 
-
-function waitForMs(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-async function deleteSentence(eleRef) {
-    const sentence = $(eleRef).html();
-    const letters = sentence.split("");
-    let i = 0;
-    while (letters.length > 0) {
-        await waitForMs(100);
-        letters.pop();
-        $(eleRef).html(letters.join(""));
-    }
-}
-
-
-const carouselText = [
-    { text: "Inform", color: "red" },
-    { text: "Build", color: "red" },
-    { text: "Strengthen", color: "red" }
-]
-
-async function carousel(carouselList, eleRef) {
-    var i = 0;
-    while (true) {
-        updateFontColor(eleRef, carouselList[i].color)
-        await typeSentence(carouselList[i].text, eleRef);
-        await waitForMs(1500);
-        await deleteSentence(eleRef);
-        await waitForMs(500);
-        i++
-        if (i >= carouselList.length) { i = 0; }
-    }
-}
-
-function updateFontColor(eleRef, color) {
-    $(eleRef).css('color', color);
-}
-
-
+window.addEventListener("load", typewriter);
