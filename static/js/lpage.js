586d47653cbc184ad0621795fe91d75e5560b9f8
Nicholi Shiell <nicholishiell@gmail.com>
2023-06-15 23:07:30 -0400

    Changes to landing page

--- static/js/lpage.js
+++ static/js/lpage.js
@@ -1,4 +1,4 @@
-var messageArray = ["Charity Insights Canada Project", "Inform", "Build", "Strengthen"];
+var messageArray = ["FILTER REPORTS USING A DATE OR THEME "];
 var textPosition = 0;
 var speed = 100;
 var speedNew = 200;
@@ -8,26 +8,12 @@ typewriter = () => {
     if (textPosition < messageArray[0].length) {
         document.querySelector('#message').innerHTML += messageArray[0][textPosition];
         textPosition++;
-    } else {
-        if (cursorVisible) {
-            document.querySelector('.cursor').style.opacity = 0;
-            cursorVisible = false;
-        } else {
-            document.querySelector('.cursor').style.opacity = 1;
-            cursorVisible = true;
-        }
     }
 
     if (textPosition < messageArray[0].length) {
         setTimeout(typewriter, speed);
-    } else if (textPosition === messageArray[0].length) {
-        setTimeout(displayRemainingText, speedNew);
     }
 }
 
-displayRemainingText = () => {
-    var remainingText = messageArray.slice(1).join("<span>\uFF0E</span>");
-    document.querySelector('#message-dynamic').innerHTML = remainingText + "<span>\uFF0E</span>";
-}
 
 window.addEventListener("load", typewriter);
