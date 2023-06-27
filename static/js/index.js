7e14d65ccb9498cc5f9bbea439153e8b3ff9fadb
Nicholi Shiell <nicholishiell@gmail.com>
2023-06-27 07:20:54 -0400

    Changes to loader

--- static/js/index.js
+++ static/js/index.js
@@ -31,3 +31,16 @@ function toggleblackbox(e) {
     }
 }
 
+function showDiv() {
+    document.getElementById('main').style.display = 'none';
+    document.getElementById('loader').style.display = 'block';
+
+    setTimeout(function () {
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('main').style.display = 'block';
+    }, 2000);
+
+})
+
+}
+
