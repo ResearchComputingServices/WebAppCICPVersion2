3a34b8caed1bf0ec8a740db945ee48b21a74073b
Nicholi Shiell <nicholishiell@gmail.com>
2023-06-27 07:30:50 -0400

    Changes to loader

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,15 @@
-window.addEventListener("load", () => {
-    const loader = document.querySelector('.loader');
+// window.addEventListener("load", () => {
+//     const loader = document.querySelector('.loader');
 
-    loader.classList.add("loader-hidden");
+//     loader.classList.add("loader-hidden");
 
-    loader.addEventListener('transitionend', () => {
-        document.removeChild("loader")
+//     loader.addEventListener('transitionend', () => {
+//         document.removeChild("loader")
 
-    })
-}
+//     })
+// }
 
-)
+// )
 function toggleblackbox(e) {
 
     e.preventDefault();
