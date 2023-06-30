2f02a3026dae15c993fe7ca64cba43ef358d4981
Nicholi Shiell <nicholishiell@gmail.com>
2023-06-30 13:17:09 -0400

    Changes in website

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -3,7 +3,7 @@ function changeLang(siteLang) {
     var link = document.getElementById("langButton");
     link.getAttribute("href");
     if (siteUrl.includes("en")) {
-        siteUrl.replace("en", "fr");
+        siteUrl = siteUrl.replace("en", "fr");
         link.setAttribute("href",
             siteUrl);
     }
@@ -16,10 +16,24 @@ function changeLang(siteLang) {
 
     else {
         if (siteLang == 'fr') {
-            siteUrl.substring(0, 21) + "/fr" + currentUrl.substring(21);
+            if (length(siteUrl) > 21) {
+                siteUrl = siteUrl.substring(0, 21) + "/fr" + currentUrl.substring(21);
+            }
+            else {
+                siteUrl = siteUrl + "/fr/"
+            }
+
         }
+
         else {
-            siteUrl.substring(0, 21) + "/en" + currentUrl.substring(21);
+            if (length(siteUrl) > 21) {
+                siteUrl.substring(0, 21) + "/en" + currentUrl.substring(21);
+            }
+            else {
+                siteUrl = siteUrl + "/en/"
+            }
         }
+        link.setAttribute("href", siteUrl);
+
     };
 };
