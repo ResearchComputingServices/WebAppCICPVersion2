6fa591bc571596c4aeca9342a94f1d19c77a8725
Nicholi Shiell <nicholishiell@gmail.com>
2023-06-30 11:02:57 -0400

    Changes in website

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,6 @@
 function changeLang(siteUrl) {
     console.log("Inside Changelang function");
-    var link = document.querySelector("a");
+    var link = document.getElementById("langButton");
     link.getAttribute("href");
     if (siteUrl.includes("/en/")) {
         siteUrl.replace("/en/", "/fr/")
