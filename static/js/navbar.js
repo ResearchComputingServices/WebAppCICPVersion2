b396342df23b77dbdf00266a47e489c0a1af1fb0
Nicholi Shiell <nicholishiell@gmail.com>
2023-07-03 22:26:31 -0400

    Changes in navbar template

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -4,34 +4,45 @@ function changeLang(siteLang) {
     link.getAttribute("href");
 
     if (siteUrl == "http://134.117.214.42/en/") {
+        console.log("case1")
         siteUrl = siteUrl.replace("http://134.117.214.42/en/", "http://134.117.214.42/fr/");
         link.setAttribute("href",
             siteUrl);
+        console.log(siteUrl)
     }
     else if (siteUrl == "http://134.117.214.42/fr/") {
-
+        console.log("case2")
         siteUrl = siteUrl.replace("http://134.117.214.42/fr/", "http://134.117.214.42/en/");
         link.setAttribute("href",
             siteUrl);
+        console.log(siteUrl)
     }
 
     else {
         if (siteLang == 'fr') {
             if (siteUrl.length > 21) {
+                console.log("case3")
                 siteUrl = siteUrl.substring(0, 21) + "/fr" + siteUrl.substring(21);
+                console.log(siteUrl)
             }
             else {
+                console.log("case4")
                 siteUrl = siteUrl + "/fr/"
+                console.log(siteUrl)
             }
 
         }
 
         else if (siteLang == 'en') {
             if (siteUrl.length > 21) {
+                console.log("case5")
                 siteUrl = siteUrl.substring(0, 21) + "/en" + siteUrl.substring(21);
+                console.log(siteUrl)
             }
             else {
+                console.log("case6")
                 siteUrl = siteUrl + "/en/"
+                console.log(siteUrl)
             }
         }
 
