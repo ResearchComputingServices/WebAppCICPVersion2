f981ec63277b77ad202cc8e0bdde3f2f7311eabf
Nicholi Shiell <nicholishiell@gmail.com>
2023-07-03 23:44:36 -0400

    Changes in navbar template

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -3,50 +3,49 @@ function changeLang(siteLang) {
     var link = document.getElementById("langButton");
     link.getAttribute("href");
 
-    if (siteUrl == "http://134.117.214.42/en/") {
-        window.alert("case1")
-        siteUrl = siteUrl.replace("http://134.117.214.42/en/", "http://134.117.214.42/fr/");
-        link.setAttribute("href",
-            siteUrl);
-        window.alert(siteUrl)
-    }
-    else if (siteUrl == "http://134.117.214.42/fr/") {
-        window.alert("case2")
-        siteUrl = siteUrl.replace("http://134.117.214.42/fr/", "http://134.117.214.42/en/");
-        link.setAttribute("href",
-            siteUrl);
-        window.alert(siteUrl)
-    }
+    // if (siteUrl.includes("http://134.117.214.42/en/")) {
+    //     window.alert("case1")
+    //     siteUrl = siteUrl.replace("http://134.117.214.42/en/", "http://134.117.214.42/fr/");
+    //     link.setAttribute("href",
+    //         siteUrl);
+    //     window.alert(siteUrl)
+    // }
+    // else if (siteUrl.includes("http://134.117.214.42/fr/")) {
+    //     window.alert("case2")
+    //     siteUrl = siteUrl.replace("http://134.117.214.42/fr/", "http://134.117.214.42/en/");
+    //     link.setAttribute("href",
+    //         siteUrl);
+    //     window.alert(siteUrl)
+    // }
 
-    else {
-        if (siteLang == 'fr') {
-            if (siteUrl.length > 30) {
-                window.alert("case3")
-                siteUrl = siteUrl.substring(0, 21) + "/fr" + siteUrl.substring(21);
-                window.alert(siteUrl)
-            }
-            else {
-                window.alert("case4")
-                siteUrl = siteUrl + "/fr/"
-                window.alert(siteUrl)
-            }
 
+    if (siteLang == 'fr' && !siteUrl.includes("http://134.117.214.42/fr/")) {
+        if (siteUrl.length > 30) {
+            window.alert("case3")
+            siteUrl = siteUrl.substring(0, 21) + "/fr" + siteUrl.substring(21);
+            window.alert(siteUrl)
+        }
+        else {
+            window.alert("case4")
+            siteUrl = siteUrl + "/fr/"
+            window.alert(siteUrl)
         }
 
-        else if (siteLang == 'en') {
-            if (siteUrl.length > 30) {
-                window.alert("case5")
-                siteUrl = siteUrl.substring(0, 21) + "/en" + siteUrl.substring(21);
-                window.alert(siteUrl)
-            }
-            else {
-                window.alert("case6")
-                siteUrl = siteUrl + "/en/"
-                window.alert(siteUrl)
-            }
+    }
+
+    else if (siteLang == 'en' && !siteUrl.includes("http://134.117.214.42/en/")) {
+        if (siteUrl.length > 30) {
+            window.alert("case5")
+            siteUrl = siteUrl.substring(0, 21) + "/en" + siteUrl.substring(21);
+            window.alert(siteUrl)
         }
+        else {
+            window.alert("case6")
+            siteUrl = siteUrl + "/en/"
+            window.alert(siteUrl)
+        }
+    }
 
-        link.setAttribute("href", siteUrl);
+    link.setAttribute("href", siteUrl);
 
-    };
 };
