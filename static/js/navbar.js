957874a4c404809b631c8db4dacffeff634829e5
Nicholi Shiell <nicholishiell@gmail.com>
2023-07-04 07:55:37 -0400

    Changes in navbar.js

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,107 +1,129 @@
+// function changeLang(siteLang) {
+//     var siteUrl = window.location.href;
+//     var link = document.getElementById("langButton");
+//     link.getAttribute("href");
+
+//     if (siteLang == 'fr') {
+//         if (siteUrl.length > 30) {
+
+//             if (siteUrl.includes("http://134.117.214.42/fr/")) {
+//                 window.alert("case1")
+//                 // siteUrl = siteUrl
+//                 window.alert(siteUrl)
+//                 link.setAttribute("href", siteUrl);
+//                 window.location.href = siteUrl;
+
+//             }
+
+//             else if (siteUrl.includes("http://134.117.214.42/en/")) {
+//                 // siteUrl = siteUrl.substring(0, 21) + "/en" + siteUrl.substring(21);
+//                 window.alert("case2")
+//                 // siteUrl = siteUrl.replace("http://134.117.214.42/fr/", "http://134.117.214.42/en/");
+//                 // link.setAttribute("href", siteUrl);
+//                 var replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)\/en/, "$1/fr");
+//                 window.alert(replacedUrl)
+//                 link.setAttribute("href", replacedUrl);
+//                 window.location.href = replacedUrl;
+
+//             }
+//         }
+//         else {
+
+//             if (siteUrl.includes("http://134.117.214.42/fr/")) {
+//                 window.alert("case3")
+//                 // siteUrl = siteUrl
+//                 window.alert(siteUrl)
+//                 link.setAttribute("href", siteUrl);
+//                 window.location.href = siteUrl;
+//             }
+
+//             else if (siteUrl.includes("http://134.117.214.42/en/")) {
+//                 // siteUrl = siteUrl.substring(0, 21) + "/en" + siteUrl.substring(21);
+//                 window.alert("case4")
+//                 // siteUrl = siteUrl.replace("http://134.117.214.42/fr/", "http://134.117.214.42/en/");
+//                 // window.alert(siteUrl)
+//                 // link.setAttribute("href", siteUrl);
+//                 var replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)\/en/, "$1/fr");
+//                 window.alert(replacedUrl)
+//                 link.setAttribute("href", replacedUrl);
+//                 window.location.href = replacedUrl;
+
+//             }
+//         }
+
+//     }
+
+//     else if (siteLang == 'en') {
+//         if (siteUrl.length > 30) {
+
+//             if (siteUrl.includes("http://134.117.214.42/en/")) {
+//                 window.alert("case4")
+//                 siteUrl = siteUrl
+//                 window.alert(siteUrl)
+//                 link.setAttribute("href", siteUrl);
+//             }
+
+//             else if (siteUrl.includes("http://134.117.214.42/fr/")) {
+//                 // siteUrl = siteUrl.substring(0, 21) + "/en" + siteUrl.substring(21);
+//                 window.alert("case5")
+//                 // siteUrl = siteUrl.replace("http://134.117.214.42/en/", "http://134.117.214.42/fr/");
+//                 // window.alert(siteUrl)
+//                 // link.setAttribute("href", siteUrl);
+//                 var replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)\/fr/, "$1/en");
+//                 window.alert(replacedUrl)
+//                 link.setAttribute("href", replacedUrl);
+//                 window.location.href = replacedUrl;
+
+//             }
+
+//         }
+//         else {
+
+//             if (siteUrl.includes("http://134.117.214.42/en/")) {
+//                 window.alert("case6")
+//                 siteUrl = siteUrl
+//                 window.alert(siteUrl)
+//                 link.setAttribute("href", siteUrl);
+
+//             }
+
+//             else if (siteUrl.includes("http://134.117.214.42/fr/")) {
+//                 window.alert("case7")
+//                 // siteUrl = siteUrl.replace("http://134.117.214.42/en/", "http://134.117.214.42/fr/");
+//                 // window.alert(siteUrl)
+//                 // link.setAttribute("href", siteUrl);
+//                 var replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)\/fr/, "$1/en");
+//                 window.alert(replacedUrl)
+//                 link.setAttribute("href", replacedUrl);
+//                 window.location.href = replacedUrl;
+
+//             }
+
+//         }
+//     }
+
+
+
+// };
+
 function changeLang(siteLang) {
     var siteUrl = window.location.href;
     var link = document.getElementById("langButton");
-    link.getAttribute("href");
 
-    if (siteLang == 'fr') {
-        if (siteUrl.length > 30) {
+    var isFrench = siteLang === 'fr';
+    var langSegment = isFrench ? '/fr' : '/en';
 
-            if (siteUrl.includes("http://134.117.214.42/fr/")) {
-                window.alert("case1")
-                // siteUrl = siteUrl
-                window.alert(siteUrl)
-                link.setAttribute("href", siteUrl);
-                window.location.href = siteUrl;
+    var replacedUrl;
 
-            }
-
-            else if (siteUrl.includes("http://134.117.214.42/en/")) {
-                // siteUrl = siteUrl.substring(0, 21) + "/en" + siteUrl.substring(21);
-                window.alert("case2")
-                // siteUrl = siteUrl.replace("http://134.117.214.42/fr/", "http://134.117.214.42/en/");
-                // link.setAttribute("href", siteUrl);
-                var replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)\/en/, "$1/fr");
-                window.alert(replacedUrl)
-                link.setAttribute("href", replacedUrl);
-                window.location.href = replacedUrl;
-
-            }
-        }
-        else {
-
-            if (siteUrl.includes("http://134.117.214.42/fr/")) {
-                window.alert("case3")
-                // siteUrl = siteUrl
-                window.alert(siteUrl)
-                link.setAttribute("href", siteUrl);
-                window.location.href = siteUrl;
-            }
-
-            else if (siteUrl.includes("http://134.117.214.42/en/")) {
-                // siteUrl = siteUrl.substring(0, 21) + "/en" + siteUrl.substring(21);
-                window.alert("case4")
-                // siteUrl = siteUrl.replace("http://134.117.214.42/fr/", "http://134.117.214.42/en/");
-                // window.alert(siteUrl)
-                // link.setAttribute("href", siteUrl);
-                var replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)\/en/, "$1/fr");
-                window.alert(replacedUrl)
-                link.setAttribute("href", replacedUrl);
-                window.location.href = replacedUrl;
-
-            }
-        }
-
+    if (siteUrl.includes('/en/')) {
+        replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)\/en/, "$1" + langSegment);
+    } else if (siteUrl.includes('/fr/')) {
+        replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)\/fr/, "$1" + langSegment);
+    } else {
+        var langUrlSegment = '/' + langSegment.substring(1) + '/';
+        replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)/, "$1" + langUrlSegment);
     }
 
-    else if (siteLang == 'en') {
-        if (siteUrl.length > 30) {
-
-            if (siteUrl.includes("http://134.117.214.42/en/")) {
-                window.alert("case4")
-                siteUrl = siteUrl
-                window.alert(siteUrl)
-                link.setAttribute("href", siteUrl);
-            }
-
-            else if (siteUrl.includes("http://134.117.214.42/fr/")) {
-                // siteUrl = siteUrl.substring(0, 21) + "/en" + siteUrl.substring(21);
-                window.alert("case5")
-                // siteUrl = siteUrl.replace("http://134.117.214.42/en/", "http://134.117.214.42/fr/");
-                // window.alert(siteUrl)
-                // link.setAttribute("href", siteUrl);
-                var replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)\/fr/, "$1/en");
-                window.alert(replacedUrl)
-                link.setAttribute("href", replacedUrl);
-                window.location.href = replacedUrl;
-
-            }
-
-        }
-        else {
-
-            if (siteUrl.includes("http://134.117.214.42/en/")) {
-                window.alert("case6")
-                siteUrl = siteUrl
-                window.alert(siteUrl)
-                link.setAttribute("href", siteUrl);
-
-            }
-
-            else if (siteUrl.includes("http://134.117.214.42/fr/")) {
-                window.alert("case7")
-                // siteUrl = siteUrl.replace("http://134.117.214.42/en/", "http://134.117.214.42/fr/");
-                // window.alert(siteUrl)
-                // link.setAttribute("href", siteUrl);
-                var replacedUrl = siteUrl.replace(/(http:\/\/134\.117\.214\.42)\/fr/, "$1/en");
-                window.alert(replacedUrl)
-                link.setAttribute("href", replacedUrl);
-                window.location.href = replacedUrl;
-
-            }
-
-        }
-    }
-
-
-
-};
+    link.setAttribute("href", replacedUrl);
+    window.location.replace(replacedUrl);
+}
