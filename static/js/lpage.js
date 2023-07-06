4d18468e87c804e6893a2108e552187d3fe924a1
Nicholi Shiell <nicholishiell@gmail.com>
2023-07-06 15:46:01 -0400

    Changes to js files

--- static/js/lpage.js
+++ static/js/lpage.js
@@ -1,4 +1,4 @@
-const messageArray = [("FILTER REPORTS USING A DATE OR THEME ")];
+const messageArray = ["FILTER REPORTS USING A DATE OR THEME "];
 let textPosition = 0;
 const speed = 100;
 const speedNew = 200;
@@ -8,12 +8,11 @@ const typewriter = () => {
     if (textPosition < messageArray[0].length) {
         document.querySelector('#message').innerHTML += messageArray[0][textPosition];
         textPosition++;
+        if (textPosition < messageArray[0].length) {
+            setTimeout(typewriter, speed);
+        }
     }
-
-    if (textPosition < messageArray[0].length) {
-        setTimeout(typewriter, speed);
-    }
-}
+};
 
 const screenloader = () => {
     const loader = document.querySelector('.loader');
@@ -21,37 +20,33 @@ const screenloader = () => {
     loader.classList.add("loader-hidden");
 
     loader.addEventListener('transitionend', () => {
-        document.removeChild("loader")
-
-    })
+        loader.parentNode.removeChild(loader);
+    });
 };
 
 window.addEventListener("load", function () {
     typewriter();
-
     // screenloader();
-
-
 });
-
 
 document.addEventListener('DOMContentLoaded', function () {
     var form = document.getElementById("myForm");
     form.onsubmit = validateDate;
 
-    function validateDate() {
+    function validateDate(event) {
         var dateInput = document.forms["myForm"]["date"].value;
+        var themeInputs = document.querySelectorAll('input[name="theme"]:checked');
 
-        if (dateInput === "") {
-            alert("Please select a date");
-            return false;
+        if (dateInput === "" && themeInputs.length === 0) {
+            alert("Please select a date and a theme to search.");
+            event.preventDefault();  // Prevent the form submission
+        } else if (dateInput === "") {
+            alert("Please select a date from the calendar to search.");
+            event.preventDefault();  // Prevent the form submission
+        } else if (themeInputs.length === 0) {
+            alert("Please select a theme to search.");
+            event.preventDefault();  // Prevent the form submission
         }
-
-        // Additional validation logic if needed
-
-        return true;  // Submit the form if validation passes
     }
 });
-
-
 
