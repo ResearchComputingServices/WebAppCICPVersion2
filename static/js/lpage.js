b874d7f806c85e73fdfb0d7732f8c611d073438e
Nicholi Shiell <nicholishiell@gmail.com>
2023-07-06 16:17:33 -0400

    Changes to js files

--- static/js/lpage.js
+++ static/js/lpage.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
     form.onsubmit = validateDate;
 
     function validateDate(event) {
-        var dateInput = "";
+        var dateInput = document.forms["myForm"]["report_date"].value;
         var themeInputs = document.querySelectorAll('input[name="theme"]:checked');
 
         if (dateInput === "" && themeInputs.length === 0) {
