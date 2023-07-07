5d03326a9a948e4a77991c0e3f5868c1163f20f9
Nicholi Shiell <nicholishiell@gmail.com>
2023-07-07 13:00:09 -0400

    Changes to RO and navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,38 +1,9 @@
-// function changeLang() {
-//   var siteUrl = window.location.href;
-//   var link = document.getElementById("langButton");
-
-//   var currentLang = getLanguageFromUrl(siteUrl);
-//   var targetLang = currentLang === "en" ? "fr" : "en";
-
-//   var langSegment = "/" + currentLang + "/";
-//   var replacedUrl = siteUrl.replace(langSegment, "/" + targetLang + "/");
-
-//   if (replacedUrl === siteUrl) {
-//     return; // No language segment replacement needed, exit function
-//   }
-
-//   link.setAttribute("href", replacedUrl);
-//   window.location.href = replacedUrl;
-// }
-
-// function getLanguageFromUrl(url) {
-//   var langSegment = url.match(/\/(en|fr)\//);
-//   if (langSegment && langSegment.length > 1) {
-//     return langSegment[1];
-//   }
-//   return ""; // Default language if no language segment found
-// }
-
-
-
 function changeLang() {
   var siteUrl = window.location.href;
   var link = document.getElementById("langButton");
 
   var currentLang = getLanguageFromUrl(siteUrl);
   var targetLang = currentLang === "en" ? "fr" : "en";
-  var altText = targetLang === "en" ? "Francias" : "English";
 
   var langSegment = "/" + currentLang + "/";
   var replacedUrl = siteUrl.replace(langSegment, "/" + targetLang + "/");
@@ -42,15 +13,13 @@ function changeLang() {
   }
 
   link.setAttribute("href", replacedUrl);
-  link.innerText = altText;
   window.location.href = replacedUrl;
+}
 
-
-  function getLanguageFromUrl(url) {
-    var langSegment = url.match(/\/(en|fr)\//);
-    if (langSegment && langSegment.length > 1) {
-      return langSegment[1];
-    }
-    return ""; // Default language if no language segment found
+function getLanguageFromUrl(url) {
+  var langSegment = url.match(/\/(en|fr)\//);
+  if (langSegment && langSegment.length > 1) {
+    return langSegment[1];
   }
-};
+  return ""; // Default language if no language segment found
+}
