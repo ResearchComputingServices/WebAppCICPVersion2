dac087a1b9c7dd80db7174bf0ab9e55200fc025d
Nicholi Shiell <nicholishiell@gmail.com>
2023-07-06 23:30:55 -0400

    Changes in js

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,22 +1,48 @@
-function changeLang(siteLang) {
+// function changeLang(siteLang) {
+//   var siteUrl = window.location.href;
+//   var link = document.getElementById("langButton");
+
+//   var isFrench = siteLang === 'fr';
+//   var langSegment = isFrench ? '/fr/' : '/en/';
+
+//   var replacedUrl;
+
+//   if (isFrench) {
+//     if (siteUrl.includes('/en/')) {
+
+//       replacedUrl = siteUrl.replace(/\/en\//, langSegment);
+//     }
+//   } else {
+
+//     replacedUrl = siteUrl.replace(/\/fr\//, langSegment);
+
+
+//   }
+//   link.setAttribute("href", replacedUrl);
+//   window.location = replacedUrl;
+// }
+function changeLang() {
   var siteUrl = window.location.href;
   var link = document.getElementById("langButton");
 
-  var isFrench = siteLang === 'fr';
-  var langSegment = isFrench ? '/fr/' : '/en/';
+  var currentLang = getLanguageFromUrl(siteUrl);
+  var targetLang = currentLang === "en" ? "fr" : "en";
 
-  var replacedUrl;
+  var langSegment = "/" + currentLang + "/";
+  var replacedUrl = siteUrl.replace(langSegment, "/" + targetLang + "/");
 
-  if (isFrench) {
-    if (siteUrl.includes('/en/')) {
-
-      replacedUrl = siteUrl.replace(/\/en\//, langSegment);
-    }
-  } else {
-
-    replacedUrl = siteUrl.replace(/\/fr\//, langSegment);
-
+  if (replacedUrl === siteUrl) {
+    return; // No language segment replacement needed, exit function
   }
+
   link.setAttribute("href", replacedUrl);
-  window.location = replacedUrl;
+  window.location.href = replacedUrl;
+}
+
+function getLanguageFromUrl(url) {
+  var langSegment = url.match(/\/(en|fr)\//);
+  if (langSegment && langSegment.length > 1) {
+    return langSegment[1];
+  }
+  return ""; // Default language if no language segment found
 }
