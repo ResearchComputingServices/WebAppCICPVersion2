edca2943c8257810f85dfbaa54d23797930326db
Nicholi Shiell <nicholishiell@gmail.com>
2023-07-18 08:46:30 -0400

    Changes in navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,21 +1,3 @@
-function changeLang() {
-  var siteUrl = window.location.href;
-  var link = document.getElementById("langButton");
-
-  var currentLang = getLanguageFromUrl(siteUrl);
-  var targetLang = currentLang === "en" ? "fr" : "en";
-
-  var langSegment = "/" + currentLang + "/";
-  var replacedUrl = siteUrl.replace(langSegment, "/" + targetLang + "/");
-
-  if (replacedUrl === siteUrl) {
-    return; // No language segment replacement needed, exit function
-  }
-
-  link.setAttribute("href", replacedUrl);
-  window.location.href = replacedUrl;
-}
-
 function getLanguageFromUrl(url) {
   var langSegment = url.match(/\/(en|fr)\//);
   if (langSegment && langSegment.length > 1) {
@@ -23,8 +5,6 @@ function getLanguageFromUrl(url) {
   }
   return ""; // Default language if no language segment found
 }
-
-
 
 function changeLang() {
   var siteUrl = window.location.href;
