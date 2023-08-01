e58031795fee0571a145406c99d6129e61492d76
Nicholi Shiell <nicholishiell@gmail.com>
2023-07-31 21:41:55 -0400

    Change in lpage files

--- static/js/lpage.js
+++ static/js/lpage.js
@@ -1,3 +1,80 @@
+// const messageArray = [gettext("FILTER REPORTS USING A DATE OR THEME")];
+// let textPosition = 0;
+// const speed = 100;
+// const speedNew = 200;
+// const cursorVisible = true;
+
+// const typewriter = () => {
+//     if (textPosition < messageArray[0].length) {
+//         document.querySelector('#message').innerHTML += messageArray[0][textPosition];
+//         textPosition++;
+//         if (textPosition < messageArray[0].length) {
+//             setTimeout(typewriter, speed);
+//         }
+//     }
+// };
+
+// const screenloader = () => {
+//     const loader = document.querySelector('.loader');
+
+//     loader.classList.add("loader-hidden");
+
+//     loader.addEventListener('transitionend', () => {
+//         loader.parentNode.removeChild(loader);
+//     });
+// };
+
+// document.onreadystatechange = function () {
+//     if (document.readyState !== "complete") {
+//         document.querySelector(
+//             "body").style.visibility = "hidden";
+//         document.querySelector(
+//             "#loader").style.visibility = "visible";
+//     } else {
+//         document.querySelector(
+//             "#loader").style.display = "none";
+//         document.querySelector(
+//             "body").style.visibility = "visible";
+//     }
+// };
+
+// window.addEventListener("load", function () {
+//     typewriter();
+//     // screenloader();
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var dateForm = document.getElementById("dateForm");
+//     var themeForm = document.getElementById("themeForm");
+
+//     dateForm.addEventListener("submit", function (event) {
+//         event.preventDefault();  // Prevent the form submission
+
+//         var dateInput = dateForm.querySelector('input[name="report_date"]').value;
+
+//         if (dateInput === "") {
+//             alert("Please select a date from the calendar to search.");
+//         } else {
+//             // If validation passes, submit the date form
+//             dateForm.submit();
+//         }
+//     });
+
+//     themeForm.addEventListener("submit", function (event) {
+//         event.preventDefault();  // Prevent the form submission
+
+//         var themeInputs = themeForm.querySelectorAll('input[name="theme"]:checked');
+
+//         if (themeInputs.length === 0) {
+//             alert("Please select a theme to search.");
+//         } else {
+//             // If validation passes, submit the theme form
+//             themeForm.submit();
+//         }
+//     });
+// });
+
+
 const messageArray = [gettext("FILTER REPORTS USING A DATE OR THEME")];
 let textPosition = 0;
 const speed = 100;
@@ -14,34 +91,20 @@ const typewriter = () => {
     }
 };
 
-// const screenloader = () => {
-//     const loader = document.querySelector('.loader');
-
-//     loader.classList.add("loader-hidden");
-
-//     loader.addEventListener('transitionend', () => {
-//         loader.parentNode.removeChild(loader);
-//     });
-// };
-
 document.onreadystatechange = function () {
     if (document.readyState !== "complete") {
-        document.querySelector(
-            "body").style.visibility = "hidden";
-        document.querySelector(
-            "#loader").style.visibility = "visible";
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
     } else {
-        document.querySelector(
-            "#loader").style.display = "none";
-        document.querySelector(
-
-            "body").style.visibility = "visible";
+        typewriter();
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
     }
 };
 
 window.addEventListener("load", function () {
-    typewriter();
-    // screenloader();
+    // All page content (including images) has been loaded, do any additional setup here
+    // You can also call your screenloader() function here if needed
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -74,5 +137,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
-
