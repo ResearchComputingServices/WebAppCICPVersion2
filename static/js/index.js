562e4c0ab90818971e3847b486968f1b34e14dc3
Nicholi Shiell <nicholishiell@gmail.com>
2023-07-31 20:44:43 -0400

    Change in css files

--- static/js/index.js
+++ static/js/index.js
@@ -10,38 +10,6 @@ window.addEventListener("load", () => {
 }
 
 )
-// function toggleblackbox(e) {
-
-//     e.preventDefault();
-//     // get the clock
-//     var myBox = document.getElementById('blackbox');
-
-//     // get the current value of the clock's display property
-//     var displaySetting = myBox.style.display;
-
-
-//     // now toggle the clock and the button text, depending on current state
-//     if (displaySetting == 'block') {
-//         // clock is visible. hide it
-//         myBox.style.display = 'none';
-//     }
-//     else {
-//         // clock is hidden. show it
-//         myBox.style.display = 'block';
-//     }
-// };
-
-// function showDiv() {
-//     document.getElementById('main').style.display = 'none';
-//     document.getElementById('loader').style.display = 'block';
-
-//     setTimeout(function () {
-//         document.getElementById('loader').style.display = 'none';
-//         document.getElementById('main').style.display = 'block';
-//     }, 60);
-
-// };
-
 
 document.addEventListener('DOMContentLoaded', function () {
     new Splide('#image-slider').mount();
