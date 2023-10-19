816dc98298bab48aa1432cb1d08e23e506ca1856
Nicholi Shiell <nicholishiell@gmail.com>
2023-10-19 07:20:02 -0400

    Changes in lpage for removing the typing effect

--- static/js/lpage.js
+++ static/js/lpage.js
@@ -1,29 +1,29 @@
-const messageArray = [gettext("FILTER REPORTS USING A DATE OR THEME")];
-let textPosition = 0;
-const speed = 100;
-const speedNew = 200;
-const cursorVisible = true;
+// const messageArray = [gettext("FILTER REPORTS USING A DATE OR THEME")];
+// let textPosition = 0;
+// const speed = 100;
+// const speedNew = 200;
+// const cursorVisible = true;
 
-const typewriter = () => {
-    if (textPosition < messageArray[0].length) {
-        document.querySelector('#message').innerHTML += messageArray[0][textPosition];
-        textPosition++;
-        if (textPosition < messageArray[0].length) {
-            setTimeout(typewriter, speed);
-        }
-    }
-};
+// const typewriter = () => {
+//     if (textPosition < messageArray[0].length) {
+//         document.querySelector('#message').innerHTML += messageArray[0][textPosition];
+//         textPosition++;
+//         if (textPosition < messageArray[0].length) {
+//             setTimeout(typewriter, speed);
+//         }
+//     }
+// };
 
-document.onreadystatechange = function () {
-    if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
-        document.querySelector("#loader").style.visibility = "visible";
-    } else {
-        typewriter();
-        document.querySelector("#loader").style.display = "none";
-        document.querySelector("body").style.visibility = "visible";
-    }
-};
+// document.onreadystatechange = function () {
+//     if (document.readyState !== "complete") {
+//         document.querySelector("body").style.visibility = "hidden";
+//         document.querySelector("#loader").style.visibility = "visible";
+//     } else {
+//         typewriter();
+//         document.querySelector("#loader").style.display = "none";
+//         document.querySelector("body").style.visibility = "visible";
+//     }
+// };
 
 document.addEventListener('DOMContentLoaded', function () {
     var dateForm = document.getElementById("dateForm");
